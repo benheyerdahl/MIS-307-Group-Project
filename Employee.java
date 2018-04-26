791900348dd3afb5e210cef4b4c05e52275d099c
Ben Heyerdahl <36636721+benheyerdahl@users.noreply.github.com>
2018-04-25 20:30:49 -0500

    final employee class

--- Employee.java
+++ Employee.java
@@ -1,14 +1,21 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Date;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-// Hello git repository
+
 /**
  * A class that contains methods for entering payroll information for each ACME employee. This class uses methods from the EmployeeRecord class in order to run and therefore, relies on the EmployeeRecord class to operate.
  */
 public class Employee
 {
+	
+
 	// ArrayLists that will implement return methods from the EmployeeRecord class.
 	ArrayList<String> employeeFirst;
 	ArrayList<String> employeeLast;
@@ -31,10 +38,13 @@ public class Employee
 
 	/**
 	 * A method that implements the EmployeeRecord class to prompt the user to enter information for the payroll of each ACME employee. The payroll for each employee is then displayed.
+	 * @throws IOException 
 	 */
-	public void setEmployeePayroll()
+	public void setEmployeePayroll() throws IOException
 	{
 
+ 
+		
 		// Constructs a new EmployeeRecord to implement classes from EmployeeRecord class.
 		employeeData e = new employeeData();
 		e.setEmployeeInfo();
@@ -52,12 +62,22 @@ public class Employee
 		int eID = 0;
 
 	while(eID != -1) {
+		DateFormat format = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
+		Date d = new Date();
+		String payDate = format.format(d);
 
-		System.out.println("Please enter employee ID: . Enter -1 for employee ID and 0 for hours when you are done inputing information.");
+    	
+		System.out.println("Please enter employee ID: . Enter 0 for employee ID and hours when you are done inputing information.");
 		eID = in.nextInt();
 		i = eID;
 		System.out.println("Enter hours worked:");
 		hoursWorked = in.nextDouble();
+		
+				
+    	File f = new File("Payroll" + employeeLast.get(eID) + employeeFirst.get(eID) + " " + payDate +  ".txt");
+    	FileWriter fr = new FileWriter(f , true);
+    	BufferedWriter bw = new BufferedWriter(fr);
+    	
 
 		if(hoursWorked != 0 || eID == -1)
 		{
@@ -85,7 +105,7 @@ public class Employee
 				tPay.add(totalPay);
 				totalPay = 0;
 				hoursWorked = 0;
-				
+
 			}
 		}
 		else {
@@ -93,16 +113,23 @@ public class Employee
 			System.exit(0);
 		}
 
-		DateFormat format = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-		Date d = new Date();
 
-		System.out.println("ACME Payroll run on " + format.format(d));
+		System.out.println("Current Payroll run on " + format.format(d));
 		i = eID;
 		System.out.println();
 		System.out.println("Employee Number  |  Employee Name    |  Hours Worked  |  Total Pay");
 		System.out.println(employeeIDs.get(eID) + "                | " + employeeFirst.get(i) + " " + employeeLast.get(i) + "        | " + hours.get(counter) + "           | " + tPay.get(counter));
-		counter++;
+		System.out.println();
+		
+		bw.write("Employee: ");
+		bw.newLine();
+		bw.write(employeeIDs.get(eID) + "                | " + employeeFirst.get(i) + " " + employeeLast.get(i) + "        | " + hours.get(counter) + " Total Hours Worked" + "           | " + tPay.get(counter) + " Gross Pay");
+		bw.newLine();
 	
+		counter++;
+		bw.close();
+		System.out.println("Check Written Successfully");
+		System.out.println();
 /**
  * Variable tester for arraylists
  * 
@@ -118,6 +145,7 @@ public class Employee
 		
 
 	}
+	
 
 	}
 
